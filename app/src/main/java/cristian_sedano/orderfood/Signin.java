4307c768bb65cc9146ea3c68a5e9185618a11dac
Christian <chris.sedano.arenas@gmail.com>
2018-02-17 18:41:45 +0000

    Add Main Screen, Login, Register and Database
    Implement de libreris, design layouts, use Firebase and coding

---
+++ app/src/main/java/cristian_sedano/orderfood/Signin.java
@@ -0,0 +1,80 @@
+package cristian_sedano.orderfood;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import cristian_sedano.orderfood.Model.User;
+
+public class Signin extends AppCompatActivity {
+
+    EditText edtPhone, edtPassword;
+    Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText) findViewById(R.id.edtPhone);
+
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        // Init FireBase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ProgressDialog mDialog = new ProgressDialog(Signin.this);
+                mDialog.setMessage("Please Waiting...");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        // Check if User not exist in database
+                        if (dataSnapshot.child(edtPhone.getText().toString()).exists()) {
+                            // Get User Information
+                            mDialog.dismiss();
+
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+
+                            if (user.getPassword().equals(edtPassword.getText().toString())) {
+                                Toast.makeText(Signin.this, "Sign In successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(Signin.this, "Wrong Password !", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(Signin.this, "User not exist in Database", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+}
